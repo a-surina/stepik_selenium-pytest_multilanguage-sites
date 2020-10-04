a648f44ed906ecf11708c25ea681dcc100d1f368
Sergey Surin <nastenka-rom@yandex.ru>
2020-10-04 18:17:52 +0300

    Time.sleep comment added

--- test_items.py
+++ test_items.py
@@ -1,9 +1,12 @@
+import time
+
 def test_button_add_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.implicitly_wait(5)
     browser.get(link)
     button = browser.find_element_by_css_selector(" .btn-add-to-basket")
 
+    # time.sleep(30)
+
     assert button.is_displayed(), "Кнопка отсутствует"
     assert button.is_enabled(), "Кнопка неактивна"
-
